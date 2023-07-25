04cf4feae284a592a950425990b1fb828e5d782e
ADMIN <minh.vcm1999@gmail.com>
2023-07-25 15:01:06 +0700

    TrangChinh

---
+++ app/src/main/java/edu/huflit/vnsapp/TrangChinh.java
@@ -0,0 +1,14 @@
+package edu.huflit.vnsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chinh);
+    }
+}
