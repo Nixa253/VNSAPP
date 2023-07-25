18df5c9be8b47b0953b5d840321be94e99f9842a
ADMIN <minh.vcm1999@gmail.com>
2023-07-25 09:21:30 +0700

    chao be le van dat

---
+++ app/src/main/java/edu/huflit/vnsapp/CapNhatTaiKhoan.java
@@ -0,0 +1,14 @@
+package edu.huflit.vnsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CapNhatTaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cap_nhat_tai_khoan);
+    }
+}
