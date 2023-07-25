08107d065fff2f9fda2eb2fd58ef571965fc7458
ADMIN <minh.vcm1999@gmail.com>
2023-07-25 13:18:06 +0700

    MaXacNhan

---
+++ app/src/main/java/edu/huflit/vnsapp/MaXacNhan.java
@@ -0,0 +1,14 @@
+package edu.huflit.vnsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaXacNhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ma_xac_nhan);
+    }
+}
